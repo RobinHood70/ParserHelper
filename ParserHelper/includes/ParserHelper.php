19666bed38e1c3a758de9c14924d041fa5fb1d76
Robin Hood <robinhood70@live.ca>
2022-07-17 14:10:15 -0400

    Show nothing if output empty

--- ParserHelper/includes/ParserHelper.php
+++ ParserHelper/includes/ParserHelper.php
@@ -164,9 +164,10 @@ class ParserHelper
 	 */
 	public static function formatPFForDebug($output, $debug = false, $noparse = false)
 	{
-		return $debug
-			? ['<pre>' . htmlspecialchars($output) . '</pre>', 'noparse' => false]
-			: [$output, 'noparse' => $noparse];
+		return
+			strlen($output) == 0 ? '' :
+			$debug ? ['<pre>' . htmlspecialchars($output) . '</pre>', 'noparse' => false] :
+			[$output, 'noparse' => $noparse];
 	}
 
 	/**
