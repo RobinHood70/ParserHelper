79d081c12ca386fd6a8315651d00033209516b2d
Robin Hood <robinhood70@live.ca>
2023-02-13 22:48:05 -0500

    Spaces to tabs

--- ParserHelper/includes/RHDebug.php
+++ ParserHelper/includes/RHDebug.php
@@ -13,11 +13,11 @@ use Wikimedia\Rdbms\ResultWrapper;
  */
 function RHalert($msg): void
 {
-    if (!RHisDev()) {
-        return;
-    }
+	if (!RHisDev()) {
+		return;
+	}
 
-    echo "<script>alert(\" $msg\")</script>";
+	echo "<script>alert(\" $msg\")</script>";
 }
 
 /**
@@ -31,12 +31,12 @@ function RHalert($msg): void
  */
 function RHformatQuery(IDatabase $db, ?ResultWrapper $result = null): string
 {
-    if (!RHisDev()) {
-        return '';
-    }
+	if (!RHisDev()) {
+		return '';
+	}
 
-    $retval = $result ? $db->numRows($result) . ' rows returned.' : '';
-    return $db->lastQuery() . "\n\n" . $retval;
+	$retval = $result ? $db->numRows($result) . ' rows returned.' : '';
+	return $db->lastQuery() . "\n\n" . $retval;
 }
 
 /**
@@ -50,12 +50,12 @@ function RHformatQuery(IDatabase $db, ?ResultWrapper $result = null): string
  */
 function RHformatQueryDbb(Database $db, ?ResultWrapper $result = null): string
 {
-    if (!RHisDev()) {
-        return '';
-    }
+	if (!RHisDev()) {
+		return '';
+	}
 
-    $retval = $result ? $db->numRows($result) . ' rows returned.' : '';
-    return $db->lastQuery() . "\n\n" . $retval;
+	$retval = $result ? $db->numRows($result) . ' rows returned.' : '';
+	return $db->lastQuery() . "\n\n" . $retval;
 }
 
 /**
@@ -68,43 +68,43 @@ function RHformatQueryDbb(Database $db, ?ResultWrapper $result = null): string
  */
 function RHlogFunctionText($text = ''): void
 {
-    if (!RHisDev()) {
-        return;
-    }
+	if (!RHisDev()) {
+		return;
+	}
 
-    $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1];
-    $method = $caller['function'];
-    if (isset($caller['class'])) {
-        $method = $caller['class'] . '::' . $method;
-    }
+	$caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1];
+	$method = $caller['function'];
+	if (isset($caller['class'])) {
+		$method = $caller['class'] . '::' . $method;
+	}
 
-    RHwriteFile($method, ': ', $text);
+	RHwriteFile($method, ': ', $text);
 }
 
 function RHlogTrace(int $limit = 0): string
 {
-    if (!RHisDev()) {
-        return '';
-    }
+	if (!RHisDev()) {
+		return '';
+	}
 
-    $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $limit == 0 ? 0 : $limit + 1);
-    if (count($trace) > 0) {
-        unset($trace[0]);
-    }
+	$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, $limit == 0 ? 0 : $limit + 1);
+	if (count($trace) > 0) {
+		unset($trace[0]);
+	}
 
-    $lines = [];
-    foreach ($trace as $index => $line) {
-        $lines[] =
-            str_pad($line['class'], 20, ' ', STR_PAD_LEFT) .
-            $line['type'] .
-            str_pad($line['function'], 25) .
-            ' on line ' .
-            str_pad($line['line'], 4, ' ', STR_PAD_LEFT) .
-            ' of file ' .
-            $line['file'];
-    }
+	$lines = [];
+	foreach ($trace as $index => $line) {
+		$lines[] =
+			str_pad($line['class'], 20, ' ', STR_PAD_LEFT) .
+			$line['type'] .
+			str_pad($line['function'], 25) .
+			' on line ' .
+			str_pad($line['line'], 4, ' ', STR_PAD_LEFT) .
+			' of file ' .
+			$line['file'];
+	}
 
-    return implode("\n", $lines);
+	return implode("\n", $lines);
 }
 
 /**
@@ -117,23 +117,23 @@ function RHlogTrace(int $limit = 0): string
  */
 function RHecho(...$msgs): void
 {
-    if (!RHisDev()) {
-        return;
-    }
+	if (!RHisDev()) {
+		return;
+	}
 
-    echo '<pre>';
-    foreach ($msgs as $msg) {
-        if ($msg) {
-            // Functions are separate for possible behaviour flags later on.
-            // The double print_r is necessary here. The first converts it to something we can capture and run
-            // htmlspecialchars on. The second one actually prints it.
-            $msg = print_r($msg, true);
-            $msg = htmlspecialchars($msg);
-            print_r($msg);
-        }
-    }
+	echo '<pre>';
+	foreach ($msgs as $msg) {
+		if ($msg) {
+			// Functions are separate for possible behaviour flags later on.
+			// The double print_r is necessary here. The first converts it to something we can capture and run
+			// htmlspecialchars on. The second one actually prints it.
+			$msg = print_r($msg, true);
+			$msg = htmlspecialchars($msg);
+			print_r($msg);
+		}
+	}
 
-    echo '</pre>';
+	echo '</pre>';
 }
 
 /**
@@ -146,12 +146,12 @@ function RHecho(...$msgs): void
  */
 function RHshow($intro, ...$msgs): void
 {
-    RHecho($intro . ': ', ...$msgs);
+	RHecho($intro . ': ', ...$msgs);
 }
 
 function RHshowBacktrace(): void
 {
-    RHecho((new Exception())->getTraceAsString());
+	RHecho((new Exception())->getTraceAsString());
 }
 
 /**
@@ -164,7 +164,7 @@ function RHshowBacktrace(): void
  */
 function RHwriteFile(...$msgs): void
 {
-    RHwriteAnyFile(RHDebug::$phLogFile, ...$msgs);
+	RHwriteAnyFile(RHDebug::$phLogFile, ...$msgs);
 }
 
 /**
@@ -178,19 +178,19 @@ function RHwriteFile(...$msgs): void
  */
 function RHwriteAnyFile(string $file, ...$msgs): void
 {
-    if (!RHisDev()) {
-        return;
-    }
+	if (!RHisDev()) {
+		return;
+	}
 
-    $handle = fopen($file, 'a') or die("Cannot open file: $file");
-    foreach ($msgs as $msg) {
-        $msg2 = print_r($msg, true);
-        fwrite($handle, $msg2);
-    }
+	$handle = fopen($file, 'a') or die("Cannot open file: $file");
+	foreach ($msgs as $msg) {
+		$msg2 = print_r($msg, true);
+		fwrite($handle, $msg2);
+	}
 
-    fwrite($handle, "\n");
-    fflush($handle);
-    fclose($handle);
+	fwrite($handle, "\n");
+	fflush($handle);
+	fclose($handle);
 }
 
 /**
@@ -201,23 +201,23 @@ function RHwriteAnyFile(string $file, ...$msgs): void
  */
 function RHisDev(): bool
 {
-    if (php_sapi_name() == 'cli') {
-        return true;
-    }
+	if (php_sapi_name() == 'cli') {
+		return true;
+	}
 
-    $server = $_SERVER['SERVER_NAME'] ?? null;
-    return in_array($server, ['content3.uesp.net', 'dev.uesp.net', 'rob-centos']);
+	$server = $_SERVER['SERVER_NAME'] ?? null;
+	return in_array($server, ['content3.uesp.net', 'dev.uesp.net', 'rob-centos']);
 }
 
 class RHDebug
 {
-    /**
-     * Where to log to for the global functions that need it.
-     */
-    public static $phLogFile = 'ParserHelperLog.txt';
+	/**
+	 * Where to log to for the global functions that need it.
+	 */
+	public static $phLogFile = 'ParserHelperLog.txt';
 
-    public static function noop()
-    {
-        // This function exists only to force PHP to include the global functions, above.
-    }
+	public static function noop()
+	{
+		// This function exists only to force PHP to include the global functions, above.
+	}
 }
--- ParserHelper/includes/VersionHelper28.php
+++ ParserHelper/includes/VersionHelper28.php
@@ -5,18 +5,18 @@
  */
 class VersionHelper28 extends VersionHelper
 {
-    public function getMagicWord(string $id): MagicWord
-    {
-        return MagicWord::get($id);
-    }
+	public function getMagicWord(string $id): MagicWord
+	{
+		return MagicWord::get($id);
+	}
 
-    public function getStripState(Parser $parser): StripState
-    {
-        return $parser->mStripState;
-    }
+	public function getStripState(Parser $parser): StripState
+	{
+		return $parser->mStripState;
+	}
 
-    public function replaceLinkHoldersText(Parser $parser, string $output): string
-    {
-        return $parser->replaceLinkHoldersText($output);
-    }
+	public function replaceLinkHoldersText(Parser $parser, string $output): string
+	{
+		return $parser->replaceLinkHoldersText($output);
+	}
 }
